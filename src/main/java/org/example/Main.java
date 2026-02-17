0e8eabade1b5e0b8e9bbb4ddcf967f9c7f884942
JRuizzz0 <javiru01ja@gmail.com>
2026-02-17 13:59:50 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+import router.RouterHandler;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", new RouterHandler());
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Servidor iniciado en http://localhost:8080");
+    }
+}
